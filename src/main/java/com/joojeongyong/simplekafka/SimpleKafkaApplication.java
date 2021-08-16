19a8db69387dcefd63379edbe9fe3c80774c42cf
jeongyong <jeongyong95@gmail.com>
2021-08-16 15:06:37 +0900

    단일 애플리케이션에서 카프카로 message pub/sub

---
+++ src/main/java/com/joojeongyong/simplekafka/SimpleKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.joojeongyong.simplekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joojeongyong/simplekafka/SimpleKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joojeongyong.simplekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
