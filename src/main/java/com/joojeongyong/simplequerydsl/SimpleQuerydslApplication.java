6406f6a1c609cb9b282beca7af90cbc92abfb004
jeongyong <jeongyong95@gmail.com>
2021-08-28 12:04:17 +0900

    Initial commit

---
+++ src/main/java/com/joojeongyong/simplequerydsl/SimpleQuerydslApplication.java
@@ -0,0 +1,13 @@
+package com.joojeongyong.simplequerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleQuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleQuerydslApplication.class, args);
+	}
+
+}
